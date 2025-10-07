35f67f1689f279ec6743a083f5c03b4a222780ff
Pratham <pathamwadhwa7@gmail.com>
2025-10-07 13:55:41 +0530

    stickyicons changes

--- src/components/StickyContact.jsx
+++ src/components/StickyContact.jsx
@@ -5,13 +5,13 @@ import { FaPhone, FaWhatsapp, FaEnvelope } from 'react-icons/fa';
 const StickyContact = () => {
   return (
     <div className="sticky-contact">
-      <a href="tel:+919910041914" className="sticky-icon">
+      <a href="tel:+919540662129" className="sticky-icon">
         <FaPhone />
       </a>
-      <a href="https://wa.me/919910041914" className="sticky-icon">
+      <a href="https://wa.me/919540662129" className="sticky-icon">
         <FaWhatsapp />
       </a>
-      <a href="mailto:info@skbgreaternoida.in" className="sticky-icon">
+      <a href="mailto:Prathamw777@gmail.com" className="sticky-icon">
         <FaEnvelope />
       </a>
     </div>
